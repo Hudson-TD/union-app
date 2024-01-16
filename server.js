ba2eb845045874e78b17472d1fb60dcb4a1c562b
Tyler Hudson <tylerhudson96@gmail.com>
2024-01-16 15:18:28 -0500

    Set channel messages to be dynamic and added data passing to client for room and users

--- server.js
+++ server.js
@@ -1,30 +1,74 @@
 const express = require("express");
+const formatMessage = require("./utils/messages");
 const http = require("http");
 const path = require("path");
 const socketio = require("socket.io");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+// Channel message provider
+const channelService = "Union Bot";
 
 // Set static assets folder
 app.use(express.static(path.join(__dirname, "public")));
 
 // Run when client connects
 io.on("connection", (socket) => {
-  socket.emit("message", "Welcome to Union!");
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
-  // Broadcast user connection
-  socket.broadcast.emit("message", "A new user has entered Union");
+    // Join appropriate room
+    socket.join(user.room);
 
-  // Client disconnect
-  socket.on("disconnect", () => {
-    io.emit("message", "A user has left Union");
+    // Welcome new user
+    socket.emit(
+      "message",
+      formatMessage(channelService, `Welcome to ${room}, ${username}!`)
+    );
+
+    // Broadcast user connection
+    socket.broadcast.emit(
+      "message",
+      formatMessage(channelService, `${username} has entered ${room}`)
+    );
+
+    // Send list of users in room
+    io.to(user.room).emit("userList", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
   });
 
   // Listen for chatMessage
   socket.on("chatMessage", (msg) => {
-    io.emit("message", msg);
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit("message", formatMessage(user.username, msg.text));
+  });
+
+  // Client disconnect
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(channelService, `${user.username} has left ${user.room}`)
+      );
+
+      // Update list of users in room
+      io.to(user.room).emit("userList", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
   });
 });
 
